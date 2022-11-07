f568a077906c23bbb863e508db349476a81d2519
dvanderblom <mijnemailadre@gmail.com>
2022-11-07 15:46:31 +0100

    final changes

--- udemy-react-course/src/components/Expenses/ExpensesChart.js
+++ udemy-react-course/src/components/Expenses/ExpensesChart.js
@@ -2,18 +2,18 @@ import Chart from '../Chart/Chart';
 
 const ExpensesChart = (props) => {
   let chartDataPoints = [
-    { label: 'Jan', value: 0 },
-    { label: 'Feb', value: 0 },
-    { label: 'Mar', value: 0 },
-    { label: 'Apr', value: 0 },
-    { label: 'May', value: 0 },
-    { label: 'Jun', value: 0 },
-    { label: 'Jul', value: 0 },
-    { label: 'Aug', value: 0 },
-    { label: 'Sep', value: 0 },
-    { label: 'Okt', value: 0 },
-    { label: 'Nov', value: 0 },
-    { label: 'Dec', value: 0 },
+    { label: 'Jan', value: 0, id: 0 },
+    { label: 'Feb', value: 0, id: 1 },
+    { label: 'Mar', value: 0, id: 2 },
+    { label: 'Apr', value: 0, id: 3 },
+    { label: 'May', value: 0, id: 4 },
+    { label: 'Jun', value: 0, id: 5 },
+    { label: 'Jul', value: 0, id: 6 },
+    { label: 'Aug', value: 0, id: 7 },
+    { label: 'Sep', value: 0, id: 8 },
+    { label: 'Okt', value: 0, id: 9 },
+    { label: 'Nov', value: 0, id: 10 },
+    { label: 'Dec', value: 0, id: 11},
   ];
 
   for (const expense of props.expenses) {
